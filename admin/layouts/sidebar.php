4c1fc0bb9c09ddc7502cd0d14130c1c3777a86ff
Lance Martija <jacobmartija03@gmail.com>
2022-07-05 16:58:07 +0800

    remove department

--- admin/layouts/sidebar.php
+++ admin/layouts/sidebar.php
@@ -34,14 +34,6 @@
             </a>
           </li>
           <li>
-            <a href="../view/department.php" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group <?= ($data['title'] == 'Department') ? 'bg-gray-100' : ''; ?>">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 transition duration-75 group-hover:text-gray-900 <?= ($data['title'] == 'Department') ? 'text-gray-900' : 'text-gray-500'; ?> viewBox=" 0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z" clip-rule="evenodd" />
-              </svg>
-              <span class="flex-1 ml-3 whitespace-nowrap">Department</span>
-            </a>
-          </li>
-          <li>
             <a href="../view/CI_Activities.php" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group <?= ($data['title'] == 'CI Activities') ? 'bg-gray-100' : ''; ?>">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 transition duration-75 group-hover:text-gray-900 <?= ($data['title'] == 'CI Activities') ? 'text-gray-900' : 'text-gray-500'; ?>" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
