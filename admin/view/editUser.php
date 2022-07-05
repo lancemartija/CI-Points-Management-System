135321691d6196e7f7d1e155eee19573f7378c5f
Lance Martija <jacobmartija03@gmail.com>
2022-07-06 02:46:25 +0800

    chore: add br

--- admin/view/editUser.php
+++ admin/view/editUser.php
@@ -27,30 +27,45 @@ $records = $display->getUser($id);
 
 ?>
 
-<form action="../includes/users.inc.php" method="POST">
+<form action=" ../includes/users.inc.php" method="POST">
     <?php foreach ($records as $data) { ?>
-        <label for="">No.</label>
-        <input type="text" name="id" value="<?php echo $id ?>" readonly>
+        <input type="hidden" name="id" value="<?php echo $id ?>" readonly>
 
         <label for="">firstname</label>
         <input type="text" name="firstname" value="<?php echo $data['first_name'] ?>">
-
+        <br>
         <label for="">middlename</label>
         <input type="text" name="middlename" value="<?php echo $data['middle_name'] ?>">
-
+        <br>
         <label for="">lastname</label>
         <input type="text" name="lastname" value="<?php echo $data['last_name'] ?>">
-
+        <br>
         <label for="">email</label>
         <input type="text" name="email" value="<?php echo $data['email'] ?>">
-
+        <br>
         <label for="">address</label>
         <input type="text" name="address" value="<?php echo $data['address'] ?>">
-
+        <br>
         <label for="">contact number</label>
         <input type="text" name="contact" value="<?php echo $data['contact_number'] ?>">
+        <br>
+        <label for="department">department</label>
+        <input type="text" name="department" value="<?php echo $data['department'] ?>">
+        <br>
+        <label for="division">division</label>
+        <select name="division" id="type" value="<?php echo $data['division'] ?>">
+            <option value="Integrated School">Integrated School</option>
+            <option value="College">College</option>
+            <option value="ASF/ASP">ASF/ASP</option>
+        </select><br>
+
+        <label for="status">status</label>
+        <select name="status" id="type" value="<?php echo $data['status'] ?>">
+            <option value="Active">Active</option>
+            <option value="Inactive">Inactive</option>
+        </select><br>
 
     <?php } ?>
     <button type="submit" name="edit">Submit</button>
 </form>
-<button type="button" onclick="location.href='../view/user.php'">Back</button>
+<button type="button" onclick="location.href='../view/users.php'">Back</button>
