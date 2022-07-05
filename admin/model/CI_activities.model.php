f374dd91717d953c3db0dbbb1eec6015f38b2dc8
Lance Martija <jacobmartija03@gmail.com>
2022-07-06 03:49:56 +0800

    update parameters

--- admin/model/CI_activities.model.php
+++ admin/model/CI_activities.model.php
@@ -2,11 +2,11 @@
 
 class CIActivities extends Dbh
 {
-    protected function setActivity($title, $date, $venue, $department, $division, $description, $type, $duration, $supervisor, $maxValue)
+    protected function setActivity($title, $date, $venue, $department, $division, $description, $type, $duration, $supervisor, $cipoints)
     {
-        $stmt = $this->connect()->prepare('INSERT INTO ci_activity (title, date, venue, department, division, description, type, duration, user_id, max_ci_points) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);');
+        $stmt = $this->connect()->prepare('INSERT INTO ci_activity (title, date, venue, department, division, description, type, duration, user_id, ci_points) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);');
 
-        if (!$stmt->execute([$title, $date, $venue, $department, $division, $description, $type, $duration, $supervisor, $maxValue])) {
+        if (!$stmt->execute([$title, $date, $venue, $department, $division, $description, $type, $duration, $supervisor, $cipoints])) {
             $stmt = null;
             header('Location: ../view/Create_activity.php?error=stmtfailed');
             exit;
@@ -19,13 +19,13 @@ class CIActivities extends Dbh
 class EditActivity extends Dbh
 {
 
-    protected function setActivity($id, $title, $date, $venue, $department, $division, $description, $type, $duration, $supervisor, $maxValue)
+    protected function setActivity($id, $title, $date, $venue, $department, $division, $description, $type, $duration, $supervisor, $cipoints)
     {
-        $stmt = $this->connect()->prepare('UPDATE ci_activity set title = ?, date = ?, venue = ?, department = ?, division = ?, description = ?, type = ?, duration = ?, user_id = ?, Max_Value_Points = ? where activity_id = ?');
+        $stmt = $this->connect()->prepare('UPDATE ci_activity SET title = ?, date = ?, venue = ?, department = ?, division = ?, description = ?, type = ?, duration = ?, user_id = ?, ci_points = ? WHERE activity_id = ?');
 
-        if (!$stmt->execute([$title, $date, $venue, $department, $division, $description, $type, $duration, $supervisor, $maxValue, $id])) {
+        if (!$stmt->execute([$title, $date, $venue, $department, $division, $description, $type, $duration, $supervisor, $cipoints, $id])) {
             $stmt = null;
-            header('Location: ../view/Create_activity.php?error=stmtfailed');
+            header('Location: ../view/CI_activityEdit.php?id=' . $id . '&error=stmtfailed');
             exit;
         };
 
