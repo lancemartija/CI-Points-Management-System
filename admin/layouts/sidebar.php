eec47b1cc36a8a906f3c33629a0a9cb98dfcd5e9
Lance Martija <jacobmartija03@gmail.com>
2022-07-06 03:49:17 +0800

    chore rename ci activity

--- admin/layouts/sidebar.php
+++ admin/layouts/sidebar.php
@@ -34,7 +34,7 @@
             </a>
           </li>
           <li>
-            <a href="../view/CI_Activities.php" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group <?= ($data['title'] == 'CI Activities') ? 'bg-gray-100' : ''; ?>">
+            <a href="../view/ciactivities.php" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group <?= ($data['title'] == 'CI Activities') ? 'bg-gray-100' : ''; ?>">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 transition duration-75 group-hover:text-gray-900 <?= ($data['title'] == 'CI Activities') ? 'text-gray-900' : 'text-gray-500'; ?>" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
               </svg>
