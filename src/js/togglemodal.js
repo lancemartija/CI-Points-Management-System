deec780828934f180f302b2c7a5c98b4bba315e2
Lance Martija <jacobmartija03@gmail.com>
2022-07-06 23:58:27 +0800

    add toggle for edit modal

--- src/js/togglemodal.js
+++ src/js/togglemodal.js
@@ -1,5 +1,7 @@
-const addBtn = document.querySelector('[data-add-button]')
 const addModal = document.querySelector('#addmodal')
+const editModal = document.querySelector('#editmodal')
+const addBtn = document.querySelector('[data-add-button]')
+const editBtn = document.querySelectorAll('[data-edit-button]')
 const closeBtn = document.querySelectorAll('[data-close-button]')
 
 addBtn.addEventListener('click', () => {
@@ -7,12 +9,23 @@ addBtn.addEventListener('click', () => {
   addModal.classList.add('flex')
 })
 
+editBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    editModal.classList.remove('hidden')
+    editModal.classList.add('flex')
+  })
+})
+
 closeBtn.forEach((btn) => {
   btn.addEventListener('click', () => {
     addModal.classList.add('hidden')
     addModal.classList.remove('flex')
+    editModal.classList.add('hidden')
+    editModal.classList.remove('flex')
   })
 })
 
 addModal.classList.add('hidden')
 addModal.classList.remove('flex')
+editModal.classList.add('hidden')
+editModal.classList.remove('flex')
