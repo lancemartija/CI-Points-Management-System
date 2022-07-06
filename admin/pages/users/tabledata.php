9a1093f62bdee9af9a415a85335e3edc6997e870
Lance Martija <jacobmartija03@gmail.com>
2022-07-06 23:57:57 +0800

    change foreach brackets

--- admin/pages/users/tabledata.php
+++ admin/pages/users/tabledata.php
@@ -28,8 +28,7 @@
             </tr>
           </thead>
           <tbody class="bg-white divide-y divide-gray-200">
-
-            <?php foreach ($records as $data) { ?>
+            <?php foreach ($records as $data) : ?>
               <tr class="hover:bg-gray-100">
                 <td class="flex items-center p-4 mr-12 space-x-6 whitespace-nowrap lg:mr-0">
                   <svg class="w-10 h-10 text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -46,16 +45,16 @@
                 <td class="p-4 text-base font-normal text-gray-900 whitespace-nowrap"><?= $data['status'] ?></td>
                 <td class="p-4 text-base font-normal text-gray-900 whitespace-nowrap"><?= $data['type'] ?></td>
                 <td class="p-4 space-x-2 whitespace-nowrap">
-                  <a href="../view/editUser.php?id=<?= $data['user_id']; ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200">
+                  <button type="button" data-edit-button data-id="<?= $data['user_id']; ?>" data-fname="<?= $data['first_name']; ?>" data-mname="<?= $data['middle_name']; ?>" data-lname="<?= $data['last_name']; ?>" data-address="<?= $data['address']; ?>" data-email="<?= $data['email']; ?>" data-contact="<?= $data['contact_number']; ?>" data-department="<?= $data['department']; ?>" data-division="<?= $data['division']; ?>" data-status="<?= $data['status']; ?>" data-type="<?= $data['type']; ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200">
                     <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                       <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
                       <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path>
                     </svg>
                     Edit user
-                  </a>
+                  </button>
                 </td>
               </tr>
-            <?php } ?>
+            <?php endforeach; ?>
           </tbody>
         </table>
       </div>
