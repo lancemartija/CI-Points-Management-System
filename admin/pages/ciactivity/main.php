322d2bcbf48e9c27bf0749fc84a23beb0be171c0
Lance Martija <jacobmartija03@gmail.com>
2022-07-06 21:48:18 +0800

    change color to green

--- admin/pages/ciactivity/main.php
+++ admin/pages/ciactivity/main.php
@@ -62,7 +62,7 @@
               </div>
             </div>
             <div class="flex items-center ml-auto space-x-2 sm:space-x-3">
-              <button type="button" data-add-button class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 sm:w-auto">
+              <button type="button" data-add-button class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-white bg-green-600 rounded-lg hover:bg-green-700 focus:ring-4 focus:ring-green-200 sm:w-auto">
                 <svg class="w-6 h-6 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
                 </svg>
