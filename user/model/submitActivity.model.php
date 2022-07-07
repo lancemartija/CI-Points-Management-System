ce29f6c673f899dee97cd48611e3b870f6208afa
Lance Martija <jacobmartija03@gmail.com>
2022-07-07 21:01:07 +0800

    update SQL query

--- user/model/submitActivity.model.php
+++ user/model/submitActivity.model.php
@@ -1,18 +1,16 @@
 <?php
 
 
-class submit extends Dbh
+class Submit extends Dbh
 {
+  protected function setSubmit($userID, $actID, $fileName, $fileType, $fileData, $numberHrs)
+  {
+    $stmt = $this->connect()->prepare('INSERT INTO user_request (user_id, activity_id, supporting_docs_name, supporting_docs_type, supporting_docs, rendered_hours, request_status) VALUES (?, ?, ?, ?, ?, ?, "pending");');
 
-    protected function setSubmit($userID, $actID, $fileName, $fileType, $fileData, $numberHrs)
-    {
-
-        $stmt = $this->connect()->prepare('INSERT into user_request (user_id, activity_id, supporting_docs_name, supporting_docs_type, supporting_docs, rendered_hours) values (?,?,?,?,?,?);');
-
-        if (!$stmt->execute([$userID, $actID, $fileName, $fileType, $fileData, $numberHrs])) {
-            $stmt = null;
-            header("location: ../view/dashboard.php?error=stmtfailed");
-            exit;
-        }
+    if (!$stmt->execute([$userID, $actID, $fileName, $fileType, $fileData, $numberHrs])) {
+      $stmt = null;
+      header("location: ../view/dashboard.php?error=stmtfailed");
+      exit;
     }
+  }
 }
