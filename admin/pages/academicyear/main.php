426bdb6be79159c30a95ffac6bd72a87f7c077e5
Lance Martija <jacobmartija03@gmail.com>
2022-07-07 17:47:04 +0800

    remove margin top

--- admin/pages/academicyear/main.php
+++ admin/pages/academicyear/main.php
@@ -5,7 +5,7 @@
   <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64">
     <main>
       <!-- HEADER -->
-      <div class="p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5">
+      <div class="p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200">
         <div class="w-full mb-1">
           <div class="mb-4">
             <nav class="flex mb-5" aria-label="Breadcrumb">
