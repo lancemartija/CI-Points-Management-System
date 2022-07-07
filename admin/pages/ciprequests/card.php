c1ba299d4c253cd5591052872f4c4d93f7d0b4b5
Lance Martija <jacobmartija03@gmail.com>
2022-07-07 21:00:15 +0800

    change message based on filters

--- admin/pages/ciprequests/card.php
+++ admin/pages/ciprequests/card.php
@@ -7,8 +7,8 @@
             <a href="#">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"><?= $data['last_name'] . ', ' . $data['first_name'] . ' ' . $data['middle_name'][0] . '.'; ?></h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700">Has <span class="font-bold"><?= $data['COUNT(ur.activity_id)']; ?></span> pending request(s).</p>
-            <a href=" #" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-yellow-500 rounded-lg hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-400/50">
+            <p class="mb-3 font-normal text-gray-700">Has <span class="font-bold"><?= $data['COUNT(ur.activity_id)']; ?></span> <?= (isset($_GET['filter'])) ? $data['request_status'] : 'total'; ?> request(s).</p>
+            <a href=" #" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-500 rounded-lg hover:bg-gray-600 focus:ring-4 focus:ring-gray-400/50">
               Read more
               <svg class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
