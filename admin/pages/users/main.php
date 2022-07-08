9bb7a23638da1b6fa65a8cbf1568474321525cf1
Lance Martija <jacobmartija03@gmail.com>
2022-07-08 22:58:49 +0800

    change variable

--- admin/pages/users/main.php
+++ admin/pages/users/main.php
@@ -85,7 +85,7 @@
       if (!empty($records)) {
         if (isset($_GET['search']) && !empty(trim($_GET['search']))) {
           $query = $_GET['search'];
-          $records = $display->getSearchData($query);
+          $records = $user->getUserSearchData($query);
 
           if (empty($records)) {
             echo '<div class="flex flex-col items-center justify-center pt-12"><svg class="w-72 h-72" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 647.63626 632.17383" xmlns:xlink="http://www.w3.org/1999/xlink">
