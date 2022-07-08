b896522210a64c2d108867c74087bc0de8b33a68
Lance Martija <jacobmartija03@gmail.com>
2022-07-08 16:04:16 +0800

    sort numerically

--- admin/pages/ciactivity/editmodal.php
+++ admin/pages/ciactivity/editmodal.php
@@ -22,7 +22,7 @@ class DisplayUserSelections extends Dbh
 
   public function getYears()
   {
-    $sql = 'SELECT * FROM academic_year;';
+    $sql = 'SELECT * FROM academic_year ORDER BY year + 0;';
     $stmt = $this->connect()->query($sql);
     $result = 0;
 
@@ -116,7 +116,7 @@ $years = $display->getYears();
             </div>
             <div class="col-span-6 sm:col-span-3">
               <label for="year" class="block mb-2 text-sm font-medium text-gray-900">Academic Year</label>
-              <select id="year" name="year" class="bg-gray-50 border border-gray-300 text-gray-900 focus:outline-none sm:text-sm rounded-lg focus:ring-gray-200 focus:ring-4 block w-full p-2.5">
+              <select id="year" name="year" class="bg-gray-50 border border-gray-300 text-gray-900 focus:outline-none sm:text-sm rounded-lg focus:ring-gray-200 focus:ring-4 block w-full p-2.5" required>
                 <option disabled selected value>Choose Academic Year</option>
                 <?php foreach ($years as $data) : ?>
                   <option value="<?= $data['ay_id'] ?>"><?= $data['year'] ?></option>
