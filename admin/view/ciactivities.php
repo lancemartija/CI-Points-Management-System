cd64b18891eba87281e673c0ca5e7e72f806c233
Lance Martija <jacobmartija03@gmail.com>
2022-07-08 22:13:23 +0800

    split data into pages

--- admin/view/ciactivities.php
+++ admin/view/ciactivities.php
@@ -5,6 +5,16 @@ $data = [
     'modal' => 'ciactivitiesmodal'
 ];
 
+$results_per_page = 20;
+
+if (isset($_GET["page"])) {
+    $page  = $_GET["page"];
+} else {
+    $page = 1;
+};
+
+$start_from = ($page - 1) * $results_per_page;
+
 session_start();
 
 if (!isset($_SESSION['userid'])) {
@@ -16,10 +26,9 @@ include_once '../database/database.php';
 
 class DisplayActivity extends Dbh
 {
-    public function getActivity()
+    public function getCount()
     {
-        $sql = 'SELECT DISTINCT ci.activity_id, ci.title, ci.date, ci.venue, ci.department, ci.division, ci.description, ci.type, ci.duration, ci.ci_points, ci.semester, ay.ay_id, ay.year, u.user_id, u.first_name, u.middle_name, u.last_name FROM ci_activity ci LEFT JOIN user u ON ci.user_id = u.user_id LEFT JOIN academic_year ay ON ci.ay_id = ay.ay_id;';
-        $stmt = $this->connect()->prepare($sql);
+        $stmt = $this->connect()->prepare('SELECT COUNT(activity_id) AS total FROM ci_activity;');
 
         if (!$stmt->execute()) {
             $stmt = null;
@@ -30,9 +39,22 @@ class DisplayActivity extends Dbh
         return $result;
     }
 
-    public function getSearchData($query)
+    public function getActivity($start_from, $results_per_page)
     {
-        $stmt = $this->connect()->prepare('SELECT * FROM ci_activity WHERE title = ? OR department = ? OR type = ?;');
+        $stmt = $this->connect()->prepare('SELECT DISTINCT ci.activity_id, ci.title, ci.date, ci.venue, ci.department, ci.division, ci.description, ci.type, ci.duration, ci.ci_points, ci.semester, ay.ay_id, ay.year, u.user_id, u.first_name, u.middle_name, u.last_name FROM ci_activity ci LEFT JOIN user u ON ci.user_id = u.user_id LEFT JOIN academic_year ay ON ci.ay_id = ay.ay_id LIMIT ' . $start_from . ', ' . $results_per_page . ';');
+
+        if (!$stmt->execute()) {
+            $stmt = null;
+            exit;
+        }
+
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function getActivitySearchData($query)
+    {
+        $stmt = $this->connect()->prepare('SELECT DISTINCT ci.activity_id, ci.title, ci.date, ci.venue, ci.department, ci.division, ci.description, ci.type, ci.duration, ci.ci_points, ci.semester, ay.ay_id, ay.year, u.user_id, u.first_name, u.middle_name, u.last_name FROM ci_activity ci LEFT JOIN user u ON ci.user_id = u.user_id LEFT JOIN academic_year ay ON ci.ay_id = ay.ay_id WHERE ci.title = ? OR ci.department = ? OR ci.type = ?;');
         $result = 0;
 
         if (!$stmt->execute([$query, $query, $query])) {
@@ -54,9 +76,10 @@ class DisplayActivity extends Dbh
     }
 }
 
-
-$display = new DisplayActivity();
-$records = $display->getActivity();
+$activity = new DisplayActivity();
+$records = $activity->getActivity($start_from, $results_per_page);
+$count = $activity->getCount();
+$total_pages = ceil($count[0]['total'] / $results_per_page);
 
 include_once '../layouts/header.php';
 include_once '../layouts/navbar.php';
