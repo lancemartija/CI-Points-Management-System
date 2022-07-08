c79b25519f7aad1891cda7661a33d9db665735b9
Lance Martija <jacobmartija03@gmail.com>
2022-07-08 18:35:28 +0800

    add fields on reject request modal

--- admin/pages/userrequests/rejectmodal.php
+++ admin/pages/userrequests/rejectmodal.php
@@ -15,6 +15,10 @@
         <h3 class="mt-5 mb-6 text-xl font-normal text-gray-500">Are you sure you want to reject this request?</h3>
         <form action="../includes/userrequests.inc.php" method="post">
           <input id="id" type="hidden" name="id" readonly>
+          <input id="userid" type="hidden" name="userid" readonly>
+          <input id="cipoints" type="hidden" name="cipoints" readonly>
+          <input id="year" type="hidden" name="year" readonly>
+          <input id="semester" type="hidden" name="semester" readonly>
           <button type="submit" name="delete" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2">
             Yes, I'm sure
           </button>
