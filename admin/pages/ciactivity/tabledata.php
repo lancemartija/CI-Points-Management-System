a20fa555035bbac51cb07d66b8d5fa41c44143c0
Lance Martija <jacobmartija03@gmail.com>
2022-07-09 16:20:02 +0800

    miscs

--- admin/pages/ciactivity/tabledata.php
+++ admin/pages/ciactivity/tabledata.php
@@ -46,7 +46,7 @@
                       <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
                       <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
                     </svg>
-                    View
+                    View activity
                   </button>
                   <button type="button" data-edit-button data-id="<?= $data['activity_id']; ?>" data-title="<?= $data['title']; ?>" data-date="<?= $data['date']; ?>" data-venue="<?= $data['venue']; ?>" data-department="<?= $data['department']; ?>" data-division="<?= $data['division']; ?>" data-description="<?= $data['description']; ?>" data-type="<?= $data['type']; ?>" data-duration="<?= $data['duration']; ?>" data-cipoints="<?= $data['ci_points']; ?>" data-year="<?= $data['ay_id']; ?>" data-semester="<?= $data['semester']; ?>" data-supervisor="<?= $data['user_id']; ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200">
                     <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
