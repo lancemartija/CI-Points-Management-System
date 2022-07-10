a2d1d3d5233594824c34533f35aa8d1eacb6c24f
Kudo24 <alistaire024@gmail.com>
2022-07-10 21:50:53 +0800

    Add: CI_activity export

--- admin/model/export.model.php
+++ admin/model/export.model.php
@@ -17,3 +17,21 @@ class ExportUser extends Dbh
         return $result;
     }
 }
+
+class ExportActivity extends Dbh
+{
+
+    protected function getActivity()
+    {
+        $stmt = $this->connect()->prepare('SELECT ci.activity_id, ci.title, ci.date, ci.venue, ci.department, ci.division, ci.description, ci.type, ci.duration, ci.ci_points, ci.semester, ay.ay_id, ay.year, u.user_id, u.first_name, u.middle_name, u.last_name FROM ci_activity ci LEFT JOIN user u ON ci.user_id = u.user_id LEFT JOIN academic_year ay ON ci.ay_id = ay.ay_id ');
+
+        if (!$stmt->execute()) {
+            $stmt = null;
+            header("location: ../view/users.php?error=stmtfailed");
+            exit;
+        }
+
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+}
