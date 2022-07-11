40a720b0e21afb6c5e68533012fff7b4177c7e03
Lance Martija <jacobmartija03@gmail.com>
2022-07-11 20:39:41 +0800

    fitx view all button

--- user/pages/ciprequests/main.php
+++ user/pages/ciprequests/main.php
@@ -55,7 +55,7 @@
                 </button>
               </form>
               <div class="flex pl-0 mt-3 space-x-1 sm:pl-2 sm:mt-0">
-                <a href="ciprequests.php?id=<?= $_GET['id']; ?><?= (isset($_GET['filter'])) ? '&filter=' . $_GET['filter'] : '' ?>" class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2 text-center;">
+                <a href="ciprequests.php?id=<?= $_SESSION['userid']; ?><?= (isset($_GET['filter'])) ? '&filter=' . $_GET['filter'] : '' ?>" class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2 text-center;">
                   <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
